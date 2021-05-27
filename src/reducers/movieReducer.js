814b49c40fa5ce50b5f3ff1488e37536d8145e3c
Richino <archerrichino@gmail.com>
2021-05-27 02:04:57 -0400

    almost finished

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -1,20 +1,30 @@
-import { ADD_MOVIE, DELETE_MOVIE } from '../actions/movieActions.js';
-import movies from './../data.js';
+import { ADD_MOVIE, DELETE_MOVIE } from "../actions/movieActions.js";
+import movies from "./../data.js";
 
-const initialState = {
-    movies: movies,
-    appTitle: "IMDB Movie Database"
-}
+export const initialState = {
+	movies: movies,
+	appTitle: "IMDB Movie Database",
+};
 
-const reducer = (state, action) => {
-    switch(action.type) {
-        case DELETE_MOVIE:
-            return {
-                movies: state.movies.filter(item=>(action.payload !== item.id))
-            }
-        default:
-            return state;
-    }
-}
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case DELETE_MOVIE:
+			return {
+				movies: state.movies.filter(item => {
+					return action.payload !== item.id;
+				}),
+			};
+		case ADD_MOVIE:
+			movies.push(action.payload);
+			let list = movies;
+			console.log("new movie", list);
+			return {
+				...state,
+				movies: list,
+			};
+		default:
+			return state;
+	}
+};
 
 export default reducer;
